cbc63c073eba325ac4d10a7f808c0145d4781b8b
dredegui <gui.pascoal@outlook.com>
2024-04-18 15:21:51 +0100

    refactor: change movement to use pygames rect

--- env/player.py
+++ env/player.py
@@ -1,18 +1,20 @@
 import pygame
+from .constants import *
 
 class Player:
     def __init__(self, name, x, y, team, color=(255, 255, 255)):
+        self.rect = pygame.Rect(x, y, PLAYER_WIDTH, PLAYER_HEIGHT)
         self.name = name
-        self.x = x
-        self.y = y
         self.y_speed = 0
         self.x_speed = 0
         self.team = team
         self.color = color
 
     def move(self):
-        self.y += self.speed
-        self.x += self.x_speed
+        self.rect.move(self.x_speed, self.y_speed)
+    
+    def undo(self):
+        self.rect.move(-self.x_speed, -self.y_speed)
 
     def render(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, 10, 50))
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
