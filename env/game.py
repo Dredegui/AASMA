689f143e3443bc6127d1625963cf637cf49737d6
dredegui <gui.pascoal@outlook.com>
2024-04-19 17:47:33 +0100

    feat: add wall and goal render

--- env/game.py
+++ env/game.py
@@ -14,7 +14,7 @@ class Game:
         ]
         self.walls = [pygame.Rect(0, 0, 800, 10), pygame.Rect(0, 0, 10, 600), pygame.Rect(0, 590, 800, 10), pygame.Rect(790, 0, 10, 600)]
         # goals are in the middle of the left and right walls
-        self.goals = [pygame.Rect(0, 295, 10, 10), pygame.Rect(790, 295, 10, 10)]
+        self.goals = [pygame.Rect(0, 250, 10, 100), pygame.Rect(790, 250, 10, 100)]
         self.ball = Ball(400, 300, 10)
         self.score = [0, 0]
         self.done = False
@@ -24,10 +24,12 @@ class Game:
     def check_winner(self):
         # if ball collides with goals, score and reset ball position
         if self.ball.rect.collidelist(self.goals) == 1: # team 1 wins
+            print("Goal scored")
             self.score[1] += 1
             self.ball = Ball(400, 300, 10)
             return 1
         elif self.ball.rect.collidelist(self.goals) == 0: # team 2 wins
+            print("Goal scored")
             self.score[0] += 1
             self.ball = Ball(400, 300, 10)
             return 2
@@ -37,10 +39,12 @@ class Game:
     def check_ball_bounce(self):
         undo_move = False
         if self.ball.rect.collidelist(self.walls) in [1, 2]: # if ball collides with top or bottom walls, reverse y_speed
+            print("Ball collision with wall")
             self.ball.undo()
             undo_move = True
             self.ball.y_speed = -self.ball.y_speed
         if self.ball.rect.collidelist(self.walls) in [0, 3]: # if ball collides with left or right walls, reverse x_speed
+            print("Ball collision with wall")
             if not undo_move:
                 self.ball.undo()
             self.ball.x_speed = -self.ball.x_speed
@@ -53,9 +57,11 @@ class Game:
             # check player collisions with walls and other players
             self.players[i].move()
             if self.players[i].rect.collidelist(self.players[:i] + self.players[i+1:]) != -1 or self.players[i].rect.collidelist(self.walls) != -1:
+                print("Player collision")
                 self.players[i].undo()
             # check player collisions with ball
             if self.players[i].rect.colliderect(self.ball.rect):
+                print("Player with Ball collision")
                 self.ball.x_speed = self.ball.rect.centerx - self.players[i].rect.centerx 
                 self.ball.y_speed = self.ball.rect.centery - self.players[i].rect.centery
         # check ball collisions with goal and walls
@@ -68,9 +74,13 @@ class Game:
         if self.screen is None:
             pygame.init()
             self.screen = pygame.display.set_mode((800, 600))
-
+        self.screen.fill((0, 0, 0))
         for player in self.players:
             player.render(self.screen)
+        for wall in self.walls:
+            pygame.draw.rect(self.screen, Colors["white"], wall)
+        for goal in self.goals:
+            pygame.draw.rect(self.screen, Colors["blue"], goal)
         self.ball.render(self.screen)
 
         pygame.display.flip()
