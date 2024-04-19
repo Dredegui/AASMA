0b6fa0ae352f6eadf7ddfb0cb51ffbc0f8db0c76
dredegui <gui.pascoal@outlook.com>
2024-04-19 19:14:38 +0100

    feat: allow env to be run with random samples

--- footpong_v0.py
+++ footpong_v0.py
@@ -1,4 +1,6 @@
 from env.footpong import footpong
+from time import sleep
+import random as r
 
 if __name__ == "__main__":
     env = footpong()
@@ -7,8 +9,21 @@ if __name__ == "__main__":
 
     done = False
     while not done:
-    #    actions = {agent: env.action_space.sample() for agent in env.agents}
-    #    _, _, done, _ = env.step(actions)
+        actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+        # get more actions of 3 for player 1 and 3
+        """
+        if (r.random() < 0.5):
+            actions['player1'] = 3
+            actions['player3'] = 3
+        # get more actions of 2 for player 2 and 4
+        if (r.random() < 0.5):
+            actions['player2'] = 2
+            actions['player4'] = 2
+        """
+        
+        #print(actions)
+        observations, rewards, done, _ = env.step(actions)
         env.render()
+        sleep(0.1)
 
     env.close()
