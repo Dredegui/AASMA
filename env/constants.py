06a81b25b250b3b2b06e9483d5c10c5a313fcfed
dredegui <gui.pascoal@outlook.com>
2024-04-20 20:13:05 +0100

    feat: add colors and refactor values

--- env/constants.py
+++ env/constants.py
@@ -3,22 +3,24 @@ COLORS = {
     'green': (0, 255, 0),
     'blue': (0, 0, 255),
     'white': (255, 255, 255),
+    'grey': (128, 128, 128),
+    'orange': (255, 165, 0),
     'black': (0, 0, 0)
 }
 DISPLAY_SIZE = (800, 600)
 # Player details
 PLAYER_SPEED = 10
 PLAYER_WIDTH = 30
-PLAYER_HEIGHT = 30
+PLAYER_HEIGHT = 40
 # Ball details
 BALL_SPEED = 16
-BALL_RADIUS = 10
+BALL_RADIUS = 8
 # Screen details 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 # Wall/Goal details
 BORDER_WIDTH = 10
-GOAL_HEIGHT = 100
+GOAL_HEIGHT = 225
 # Game details
 MAX_SCORE = 5
 # Movement details
