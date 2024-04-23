816497e5a4811411aaf797dbefe77abd2dd2a03c
dredegui <gui.pascoal@outlook.com>
2024-04-23 19:48:09 +0100

    feat: implement a Deep Q Learning Network

---
+++ dqn.py
@@ -0,0 +1,110 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import random
+import numpy as np
+import gymnasium as gym
+from collections import namedtuple, deque
+
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class Net(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size=128, ):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        nn.init.normal_(self.fc1.weight, mean=0., std=0.1)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        nn.init.normal_(self.fc2.weight, mean=0., std=0.1)
+        self.out = nn.Linear(hidden_size, output_size)
+        nn.init.normal_(self.out.weight, mean=0., std=0.1)
+
+    def forward(self, state):
+        state = F.relu(self.fc1(state))
+        state = F.relu(self.fc2(state))
+        state = self.out(state)
+        return state
+    
+    def save(self, path):
+        torch.save(self.state_dict(), path)
+    
+class DQN():
+    def __init__(self, player, gamma=0.99, lr=0.001, epsilon=0.1, len_observation_space=5, len_action_space=5, device="cpu"):
+        self.player = player
+        self.path = f"models/{player}.pt"
+        self.gamma = gamma
+        self.lr = lr
+        self.epsilon = epsilon
+        self.len_observation_space = len_observation_space
+        self.len_action_space = len_action_space
+        self.batch_size = 128 # TODO review this 
+        self.device = device
+        self.model = Net().to(self.device)
+        self.target_model = Net().to(self.device)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        self.loss = nn.MSELoss()
+        self.memory = deque(maxlen=10000)
+
+    def choose_action(self, state):
+        # print state shape
+        print(state.shape)
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(self.len_action_space)
+        with torch.no_grad():
+            state = torch.tensor(state, dtype=torch.float32).to(self.device)
+            action_value = self.model(state)
+            return action_value.max(1).indices.view(1, 1)
+    
+    def push(self, state, action, next_state, reward):
+        self.memory.append(Transition(state, action, next_state, reward))
+
+    def learn(self):
+        if len(self.memory) < self.batch_size:
+            return
+        transitions = random.sample(self.memory, self.batch_size)
+        # This converts batch-array of Transitions to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=self.device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+        # TODO is this necessary?
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        state_action_values = self.model(state_batch).gather(1, action_batch)
+        next_state_values = torch.zeros(self.batch_size, device=self.device)
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.target_model(non_final_next_states).max(1).values
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+
+        # Compute Huber loss
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.model.parameters(), 100)
+        self.optimizer.step()
+
+    def update_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+
+    def soft_update_target_model(self, tau=0.01):
+        target_dict = self.target_model.state_dict()
+        model_dict = self.model.state_dict()
+        for key in model_dict.keys():
+            target_dict[key] = (1 - tau) * target_dict[key] + tau * model_dict[key]
+        self.target_model.load_state_dict(target_dict)
+
+    def decay_epsilon(self):
+        self.epsilon = max(0.1, self.epsilon - 0.0001)
+
+    def save_target(self):
+        self.target_model.save(self.path)
+
+    
--- footpong_v0.py
+++ footpong_v0.py
@@ -2,13 +2,29 @@ import env.footpong
 from env.constants import *
 from time import sleep
 from sys import argv
+import math
+import random
+import numpy as np
+import torch
+import matplotlib
+import matplotlib.pyplot as plt
+from dqn import DQN
+from itertools import count
 import random as r
 import pygame
+
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+plt.ion()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if __name__ == "__main__":
     env = env.footpong.footpong(render_mode="human")
     env.reset()
     env.render()
+    dqns = [DQN(f"player{i}", device=device) for i in range(1, 5)]
+    # models = [q_learning(env, player=f"player{i}") for i in range(1, 5)]
 
     user_mode = NO_USER
     if len(argv) == 2 and argv[1] in ['1', '2', '3', '4']:
@@ -21,42 +37,55 @@ if __name__ == "__main__":
         user_mode = TWO_USER
 
     clock = pygame.time.Clock()
-    while env.agents:
-        if user_mode:
-            actions = {}
+    episodes = 1000
+    while episodes > 0:
+        observations, _ = env.reset()
+        observations ={agent: torch.tensor(observations[agent], dtype=torch.float32, device=device).unsqueeze for agent in env.agents}
+        while env.agents:
+            if user_mode:
+                actions = {}
+                for agent in env.agents:
+                    keys = pygame.key.get_pressed()
+                    if agent == f"player{argv[1]}":
+                        if keys[pygame.K_UP]:
+                            actions[agent] = MOVE_UP
+                        elif keys[pygame.K_DOWN]:
+                            actions[agent] = MOVE_DOWN
+                        elif keys[pygame.K_LEFT]:
+                            actions[agent] = MOVE_LEFT
+                        elif keys[pygame.K_RIGHT]:
+                            actions[agent] = MOVE_RIGHT
+                        else:
+                            actions[agent] = DONT_MOVE
+                    elif user_mode == TWO_USER and agent == f"player{argv[2]}":
+                        if keys[pygame.K_w]:
+                            actions[agent] = MOVE_UP
+                        elif keys[pygame.K_s]:
+                            actions[agent] = MOVE_DOWN
+                        elif keys[pygame.K_a]:
+                            actions[agent] = MOVE_LEFT
+                        elif keys[pygame.K_d]:
+                            actions[agent] = MOVE_RIGHT
+                        else:
+                            actions[agent] = DONT_MOVE
+                    else:
+                        actions[agent] = DONT_MOVE
+                clock.tick(30)
+            else:
+                actions = {f"player{i}": dqns[f"player{i}"].choose_action(observations[f"player{i}"]) for i in range(1, 5)}
+
+            next_observations, rewards, terminations, truncations, infos = env.step(actions)
+            loss = 0
             for agent in env.agents:
-                keys = pygame.key.get_pressed()
-                if agent == f"player{argv[1]}":
-                    if keys[pygame.K_UP]:
-                        actions[agent] = MOVE_UP
-                    elif keys[pygame.K_DOWN]:
-                        actions[agent] = MOVE_DOWN
-                    elif keys[pygame.K_LEFT]:
-                        actions[agent] = MOVE_LEFT
-                    elif keys[pygame.K_RIGHT]:
-                        actions[agent] = MOVE_RIGHT
-                    else:
-                        actions[agent] = DONT_MOVE
-                elif user_mode == TWO_USER and agent == f"player{argv[2]}":
-                    if keys[pygame.K_w]:
-                        actions[agent] = MOVE_UP
-                    elif keys[pygame.K_s]:
-                        actions[agent] = MOVE_DOWN
-                    elif keys[pygame.K_a]:
-                        actions[agent] = MOVE_LEFT
-                    elif keys[pygame.K_d]:
-                        actions[agent] = MOVE_RIGHT
-                    else:
-                        actions[agent] = DONT_MOVE
-                else:
-                    actions[agent] = DONT_MOVE
-            clock.tick(30)
-        else:
-            # choose random actions for all agents
-            actions = {agent: env.action_space(agent).sample() for agent in env.agents}
-            clock.tick(30)
-
-        observations, rewards, terminations, truncations, infos = env.step(actions)
-        env.render()
-
+                loss += models[env.agent_name_mapping[agent]].learn(observations[agent], actions[agent], rewards[agent], terminations[agent] or truncations[agent], next_observations[agent])
+            print(f"Average Loss: {loss/len(env.agents)}")
+            observations = next_observations
+            env.render()
+        for model in models:
+            model.decay_epsilon()
+        episodes -= 1
+    
+    if user_mode == NO_USER:
+        for model in models:
+            model.save_q_values()
     env.close()
