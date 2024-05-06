fix: normalization reference value was changing observation value

dqn.py
@@ -27,14 +27,16 @@ class Net(nn.Module):
 
     def forward(self, state):
         # normalize the state
-        state[:, 0::2] /= 800
-        state[:, 1::2] /= 600
+        # shallow copy the state
+        sc_state = state
+        sc_state[:, 0::2] /= 800
+        sc_state[:, 1::2] /= 600
 
-        state = F.relu(self.fc1(state))
-        state = F.relu(self.fc2(state))
-        state = F.relu(self.fc3(state))
-        state = self.out(state)
-        return state
+        sc_state = F.relu(self.fc1(sc_state))
+        sc_state = F.relu(self.fc2(sc_state))
+        sc_state = F.relu(self.fc3(sc_state))
+        sc_state = self.out(sc_state)
+        return sc_state
     
     def save(self, path):
         if not os.path.exists("models"):
@@ -48,7 +50,7 @@ class Net(nn.Module):
             self.to(device)
     
 class DQN():
-    def __init__(self, player, gamma=0.995, lr=0.001, epsilon=0.9, len_observation_space=10, len_action_space=5, device="cpu"):
+    def __init__(self, player, gamma=0.995, lr=0.001, epsilon=0.1, len_observation_space=10, len_action_space=5, device="cpu"):
         self.player = player
         self.path = f"models/{player}.pt"
         self.gamma = gamma