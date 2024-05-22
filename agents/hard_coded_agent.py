a8e0af50dafd6ac36b365f0040aeb0bac77a4b73
dredegui <gui.pascoal@outlook.com>
2024-05-22 21:01:08 +0100

    fix: right team policy imbalance

--- agents/hard_coded_agent.py
+++ agents/hard_coded_agent.py
@@ -12,14 +12,16 @@ class HardCodedAgent(Agent):
 
 	def _corner_case(self, observation):
 		# check if the ball is in the corner
-		ball = observation[-2:].cpu()
-		player = observation[(self.agent_id-1)*2:(self.agent_id-1)*2+2].cpu()
+		ball = observation[-2:]
+		player = observation[(self.agent_id-1)*2:(self.agent_id-1)*2+2]
+		if self.device is not None:
+			ball = ball.cpu()
+			player = player.cpu()
 		distance = np.linalg.norm(np.array(player) - np.array(ball))			
-		max_distance = 3*PLAYER_WIDTH 
+		max_distance = 4*PLAYER_WIDTH 
 		if distance > max_distance:
 			return None
-		here = True
-		if True:
+		if rnd.random() < 0.5:
 			if (ball[0] > SCREEN_WIDTH - max_distance and not(max_distance < ball[1] < SCREEN_HEIGHT - max_distance)):
 				return MOVE_LEFT
 			if (ball[0] < max_distance and not(max_distance < ball[1] < SCREEN_HEIGHT - max_distance)):
@@ -52,7 +54,7 @@ class HardCodedAgent(Agent):
 			return DONT_MOVE
 		ball = observation[-2:]
 		player = observation[(self.agent_id-1)*2:(self.agent_id-1)*2+2]
-		if player[0] >= ball[0]:
+		if player[0] >= ball[0] + BALL_DIAMETER:
 			if abs(player[1]-ball[1]) < BALL_DIAMETER:
 				return MOVE_LEFT
 			elif player[1] < ball[1]:
